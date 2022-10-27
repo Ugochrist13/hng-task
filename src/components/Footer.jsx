5cc5b9b10bc8c30115937861b56972855e1b873c
Christian <christianugoo@gmail.com>
2022-10-27 17:54:36 +0100

    some changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,20 @@
-import { Flex } from '@chakra-ui/react'
-import React from 'react'
+import { Flex } from "@chakra-ui/react";
+import React from "react";
 
 function Footer() {
   return (
-    <Flex>
-      <Text></Text>
+    <Flex gap="100px" pt="100px" justify="center" alignItems="center">
+      <Text>
+        Zuri<span>.</span>Internship
+      </Text>
+      <Text>HNG Internship 9 Frontend Task</Text>
+      <Box
+        bg="url('../assets/img/ifg.svg')"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+      />
     </Flex>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,8 +1,10 @@
-import { Button, VStack } from "@chakra-ui/react";
+import { Box, Button, Flex, VStack, Text } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
+import '../assets/img/ifg.svg'
 
 function Links() {
   return (
+    <>
     <VStack w="100%" pt="50px" gap="20px">
       <VStack h="76px" bg="gray.200" w="100%" p="24px 32px" borderRadius="10px">
         <Button id="btn__zuri" colorScheme="gray.900" fontSize="18px" h="28px" color="black">
@@ -30,6 +32,13 @@ function Links() {
         </Button>
       </VStack>
     </VStack>
+    <Flex gap="100px" pt="100px" justify="center" alignItems="center" >
+      <Text>Zuri<span>.</span>Internship</Text>
+      <Text>HNG Internship 9 Frontend Task</Text>
+      <Box bg="url('../assets/img/ifg.svg')"   bgPosition="center"
+  bgRepeat="no-repeat" />
+    </Flex>
+    </>
   );
 }
 
