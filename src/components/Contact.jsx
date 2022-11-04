a715ad63596a81b8cf64f9b218ece32c479e747b
Christian <christianugoo@gmail.com>
2022-11-04 17:27:04 +0100

    stage 2 push

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,59 @@
 import React from "react";
-import { Form, useSubmit } from "react-router-dom";
+import { Form, useNavigate, useSubmit } from "react-router-dom";
 
 function Contact() {
-const submit = useSubmit()
+  const submit = useSubmit();
+  const navigate = useNavigate();
 
   return (
     <div>
-      <Form method="post" onChange={(event) => {
-        submit(event.currentTarget);
-      }}>
-        <label for="name" name="First name"> First Name
-          <input type="text" id="first_name" name="First Name" placeholder="e.g Christian" />
+      <Form
+        method="post"
+        action="/"
+        onChange={(event) => {
+          submit(event.currentTarget);
+        }}
+      >
+        <label for="name" name="First name">
+          {" "}
+          First Name
+          <input
+            type="text"
+            id="first_name"
+            name="First Name"
+            placeholder="e.g Christian"
+          />
         </label>
-        <br/>
+        <br />
         <label for="name" name="Last name">
-          <input type="text" id="last_name" name="Last Name" placeholder="e.g Nwabiukwu" />
+          <input
+            type="text"
+            id="last_name"
+            name="Last Name"
+            placeholder="e.g Nwabiukwu"
+          />
         </label>
-        <br/>
+        <br />
         <label for="" name="Email">
-          <input type="text" id="email" name="Email" placeholder="e.g Christian" />
+          <input
+            type="text"
+            id="email"
+            name="Email"
+            placeholder="e.g Christian"
+          />
         </label>
-        <br/>
+        <br />
         <textarea id="message" name="Message"></textarea>
-        <br/>
-        <button type="submit" id="btn__submit">Submit</button>
+        <br />
+        <button
+          type="submit"
+          id="btn__submit"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Submit
+        </button>
       </Form>
     </div>
   );
